7d2db543d29f8c3afcce5c54041ef71d63f3bd7b
evelin-code <f.gongoraandrade@gmail.com>
2024-07-26 18:12:09 -0500

    Fake data is created

---
+++ src/DATOS_FAKE/products.js
@@ -0,0 +1,66 @@
+export const products = [
+  {
+    id: 1,
+    name: "Computador Portatil Asus 1",
+    price: 10000,
+    image: "https://instrumentalia.com.co/43218-large_default/computador-portatil-asus-zenbook-ux482-procesador-intelk-core-i7-1165g7-disco-solido-512gb.jpg",
+    description: "lorem (...)",
+    stock: 10
+  },
+  {
+    id: 2,
+    name: "Computador Portatil Asus 2",
+    price: 10000,
+    image: "https://instrumentalia.com.co/43218-large_default/computador-portatil-asus-zenbook-ux482-procesador-intelk-core-i7-1165g7-disco-solido-512gb.jpg",
+    description: "lorem (...)",
+    stock: 10
+  },
+  {
+    id: 3,
+    name: "Computador Portatil Asus 3",
+    price: 10000,
+    image: "https://instrumentalia.com.co/43218-large_default/computador-portatil-asus-zenbook-ux482-procesador-intelk-core-i7-1165g7-disco-solido-512gb.jpg",
+    description: "lorem (...)",
+    stock: 10
+  },
+  {
+    id: 4,
+    name: "Computador Portatil Asus 4",
+    price: 10000,
+    image: "https://instrumentalia.com.co/43218-large_default/computador-portatil-asus-zenbook-ux482-procesador-intelk-core-i7-1165g7-disco-solido-512gb.jpg",
+    description: "lorem (...)",
+    stock: 10
+  },
+  {
+    id: 4,
+    name: "Computador Portatil Asus 4",
+    price: 10000,
+    image: "https://instrumentalia.com.co/43218-large_default/computador-portatil-asus-zenbook-ux482-procesador-intelk-core-i7-1165g7-disco-solido-512gb.jpg",
+    description: "lorem (...)",
+    stock: 10
+  },
+  {
+    id: 4,
+    name: "Computador Portatil Asus 4",
+    price: 10000,
+    image: "https://instrumentalia.com.co/43218-large_default/computador-portatil-asus-zenbook-ux482-procesador-intelk-core-i7-1165g7-disco-solido-512gb.jpg",
+    description: "lorem (...)",
+    stock: 10
+  },
+  {
+    id: 4,
+    name: "Computador Portatil Asus 4",
+    price: 10000,
+    image: "https://instrumentalia.com.co/43218-large_default/computador-portatil-asus-zenbook-ux482-procesador-intelk-core-i7-1165g7-disco-solido-512gb.jpg",
+    description: "lorem (...)",
+    stock: 10
+  },
+  {
+    id: 4,
+    name: "Computador Portatil Asus 4",
+    price: 10000,
+    image: "https://instrumentalia.com.co/43218-large_default/computador-portatil-asus-zenbook-ux482-procesador-intelk-core-i7-1165g7-disco-solido-512gb.jpg",
+    description: "lorem (...)",
+    stock: 10
+  }
+]
